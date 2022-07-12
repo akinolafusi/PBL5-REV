34a7e4e2f844c89cc0812c279284e1e718433300
akinola <olafusiakinwumi@gmail.com>
2022-07-13 00:06:17 +0100

    project5 start

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "projectSG" {
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    name = "ProjectSG"
+  }
+}
+
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "UBUNTU2.pem"
+#   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC/WKL12Ww7EWuKirBs5kfnT4FG6TU2D9a6dxoNb7j+XljCuvubO//midnxRasWyaEauX0LpC7PPokRLFsy2aNPteq1gBRzdfcgnfgDTVcpxFo+0d4XrK64u5oc9nNXmg0YzqOI4kQ2QaLBc6NB/5qjN1IS72PRYjVG1hHUy9evV7liIIBKQcQ/zDDsuab1W4v1VNkd7TI9WTSeNyHB+12IRuumd63rBj2vuiy0DBh9eQj/c60BnuCX5w7LXv6GRyFyS3kYm6NohGf5y72dcTZPRQTBSFI3DaI1APRL0lf3kcwL1jSiI2qfEOcet0jk7C041ZqVmFXw99AaRsG1DQQcis6WjRwNGAYk6OirYadE3eOrNKDj09NwuDFWL/KvEOqA1MQwEfzVNxCJ5RjjqC9tO5dFC1EbburW5K/OKlpL+RB1R2TEskP7twfFpKyqV1Hi7w91XChHga97TwsjABz3GzFr8Bu1tKG9s5jFAOFXuN2sZ2rzOPupx9N2udQuxQc= root@DESKTOP-AKKC1GS"
+
+# }
+
+resource "aws_instance" "projectone" {
+  ami             = "ami-052efd3df9dad4825"
+  instance_type   = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.projectSG.id}"]
+  key_name        = "UBUNTU2"
+  tags = { 
+
+    Name = "PROJECT4"
+
+  }
+}
